d3149ca827d2a881dc05f915f50533e55731d974
FalconHH <103220346+FalconHH@users.noreply.github.com>
2024-07-21 23:08:59 +0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local discordWebhookURLlogin = "https://discord.com/api/webhooks/1262811919551828069/wOWa9bMzGWiNiYHm5lwHE2SdVTNpVxLI1UXRRk4u1Sd5JURYfqrAOxYD5_PLk_vf-WF6"
-local discordWebhookURLchat = "https://discord.com/api/webhooks/1262811482513604680/m9TtDwoMBNDgJazHYTes1VgHqou8dM1EU-q9R_Itk5CbxTq_NYCK8-4LI2aBjqB5xT9m"
+local discordWebhookURLlogin = "your_webhook"
+local discordWebhookURLchat = "your_webhook"
 
 
 local function login(name, message, color)
@@ -31,34 +31,24 @@ local function chat(name, message, color)
     PerformHttpRequest(discordWebhookURLchat, function(err, text, headers) end, 'POST', json.encode({username = "Server Bot", embeds = connect}), { ['Content-Type'] = 'application/json' })
 end
 
--- Event when a player joins
+
 AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
     local playerName = GetPlayerName(source)
-    local message = playerName .. " Sunucuya giriş yaptı."
-    login("Oyuncu Katıldı", message, 65280) -- Green color
+    local message = playerName .. " Joined server"
+    login("Player Joined", message, 65280) 
 end)
 
--- Event when a player disconnects
+
 AddEventHandler('playerDropped', function(reason)
     local playerName = GetPlayerName(source)
-
-    if string.sub(reason, 1, 29) == "[txAdmin] Sunucu yeniden başlatılıyor." then
-        login("Sunucu yeniden başlatılıyor", "Sunucu yeniden başlatılıyor.", 16776960) -- Yellow color
-    else
-        local message = playerName .. " Sunucudan ayrıldı. Sebep: " .. reason
-        login("Oyuncu Ayrıldı", message, 16711680) -- Red color
-    end
+    local message = playerName .. " Left server. Reason: " .. reason
+    login("Player Left", message, 16711680)
 end)
 
--- Event for chat messages
-AddEventHandler('chatMessage', function(source, name, message)
-    -- Prevent the default chat message
-    CancelEvent()
 
-    -- Format the message to be sent to Discord
+AddEventHandler('chatMessage', function(source, name, message)
+    CancelEvent()
     local playerName = GetPlayerName(source)
     local messageContent = playerName .. ": " .. message
-
-    chat("Chat Message", messageContent, 3447003) -- Blue color (you can choose your own color)
-
+    chat("Chat Message", messageContent, 3447003) 
 end)
